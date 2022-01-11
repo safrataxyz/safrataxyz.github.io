72528070ef1cbb00e8e659bbb14343eaba1a1d0e
safrata <safrata@protonmail.ch>
2022-01-11 15:12:55 +0100

    added front page, contact page

---
+++ js/contact_script.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', (event) => {
+    var mail_a = document.getElementById("mail");
+    var body_el = document.querySelector("body");
+    mail_a.addEventListener('click', (event) => {
+        navigator.clipboard.writeText("safrata@protonmail.com");
+        var hint_p = document.createElement("p");
+        hint_p.className = "hint";
+        hint_p.innerHTML = "copied to clipboard"
+        hint_p.style.fontSize = '32px';
+        hint_p.style.textDecoration = 'none';
+        hint_p.style.zIndex = '2'
+        hint_p.style.padding = '5px'
+        hint_p.style.border = '1px solid #CC7722'
+        hint_p.style.backgroundColor = '#000000';
+        hint_p.style.position = 'fixed'
+        hint_p.style.width = '227px';
+        hint_p.style.left = '50%';
+        hint_p.style.marginLeft = '-119px';
+        hint_p.style.top = '1.5vh';
+        body_el.appendChild(hint_p);
+        hint_p.addEventListener('animationend', () => {
+            hint_p.remove()
+        })
+    })
+});
+
