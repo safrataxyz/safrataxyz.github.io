a1a4627ff1568f38b8ffa355af48385a043fa27f
Jakub Safrata <jakub.safrata@protonmail.ch>
2024-01-02 11:42:34 +0100

    feat: create a next deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="container pt-12">
       <h1 className="font-sans text-8xl font-bold">Jakub Safrata</h1>
+      <h2 className="font-sans text-4xl font-bold">developer, builder</h2>
     </main>
   );
 }
